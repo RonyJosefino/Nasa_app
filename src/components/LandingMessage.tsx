641de525af4a58ad1707b9fc719e9ef56193b19a
JoaoFernandoHelios <joaofrguimaraes1@gmail.com>
2025-10-05 20:57:09 -0300

    Update landing message title to "Cosmic quest"

--- src/components/LandingMessage.tsx
+++ src/components/LandingMessage.tsx
@@ -5,7 +5,7 @@ import './LandingMessage.css'; // Vamos criar este CSS a seguir
 const LandingMessage: React.FC = () => {
   return (
     <div className="content">
-      <h1>Embiggen Your Eyes</h1>
+      <h1>Cosmic quest</h1>
       <div className="line"></div>
       <p>Explore outer space in a way</p>
       <p>you've never seen before</p>
